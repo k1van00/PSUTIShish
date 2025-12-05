43fdf3b685157ee26be5adb300ead806d0adb105
Alex <eks1elent@yandex.ru>
2025-12-06 00:04:43 +0400

    новые названия переменных

--- hello_mir.py
+++ hello_mir.py
@@ -1,6 +1,10 @@
 #НОВЫЙ.КОММЕНТАРИЙ.ОФИЦИАЛЬНО.
 import time
+<<<<<<< HEAD
 def privet_s_imenem():  
+=======
+def privetik_s_imenem():  
+>>>>>>> 83ebba8 (новые названия переменных)
     imya = input("Введите ваше имя: ")  
     print(f"Hello world from {imya}")
     time.sleep(10)

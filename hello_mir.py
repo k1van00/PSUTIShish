5b970b84d2990532e054f2b03f54e2c1a90869df
Alex <148830650+k1van00@users.noreply.github.com>
2025-12-05 21:53:00 +0400

    Update hello_mir.py

--- hello_mir.py
+++ hello_mir.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+import time
+def console_greeting():  
+    name = input("Введите ваше имя: ")  
+    print(f"Hello world from {name}")
+    time.sleep(10)
+console_greeting()
